87d75de86b830e54eee191a91381a07254134c03
Idnk <efeidukpaye@gmail.com>
2026-01-01 12:30:30 +0100

    feat(story-1.2): set up Prisma with SQLite and Book model

---
+++ server/prisma/migrations/20260101112552_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
