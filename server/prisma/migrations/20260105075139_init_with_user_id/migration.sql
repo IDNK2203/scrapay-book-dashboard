4d4a84b3e2be88c0ffbe909f363afd3dfc9b6698
Idnk <efeidukpaye@gmail.com>
2026-01-05 08:54:58 +0100

    fix(story-4.3): recreate migration with user_id column for Render deployment

--- server/prisma/migrations/20260105075139_init_with_user_id/migration.sql
+++ server/prisma/migrations/20260105075139_init_with_user_id/migration.sql
@@ -1,6 +1,7 @@
 -- CreateTable
 CREATE TABLE "books" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "author" TEXT NOT NULL,
     "description" TEXT,
